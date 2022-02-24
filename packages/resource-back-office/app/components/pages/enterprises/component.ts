d54f3d92e60979e139c302efcef42e8db6f1abd5
LouisFitdevoie <fitdevoielouis@hotmail.be>
2022-02-24 10:24:26 +0100

    Enterprises page layout created

---
+++ packages/resource-back-office/app/components/pages/enterprises/component.ts
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+
+interface PagesEnterprisesArgs {}
+
+// eslint-disable-next-line ember/no-empty-glimmer-component-classes
+export default class PagesEnterprises extends Component<PagesEnterprisesArgs> {}
