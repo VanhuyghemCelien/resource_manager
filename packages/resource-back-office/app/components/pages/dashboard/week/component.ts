98d029b75f42784858df0018f45c42723fffea6c
Sacha <sahiouni.sacha@outlook.com>
2022-02-25 16:45:55 +0100

    dashboard-week 1.2

--- packages/resource-back-office/app/components/pages/dashboard/week/component.ts
+++ packages/resource-back-office/app/components/pages/dashboard/week/component.ts
@@ -1,8 +1,46 @@
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { format, startOfWeek } from 'date-fns';
 
 interface PagesDashboardWeekArgs {}
 
 export default class PagesDashboardWeek extends Component<PagesDashboardWeekArgs> {
+  today: Date = new Date();
+  @tracked choosingDay: Date = new Date();
+  @tracked specificDay: Date = new Date();
+
+  @action
+  choosingDayPlus() {
+    this.choosingDay = new Date(
+      this.choosingDay.setDate(this.choosingDay.getDate() + 7)
+    );
+  }
+
+  @action
+  choosingDayLess() {
+    this.choosingDay = new Date(
+      this.choosingDay.setDate(this.choosingDay.getDate() - 7)
+    );
+  }
+
+  @action
+  choosingDayActual() {
+    this.choosingDay = new Date();
+    this.specificDay = new Date();
+  }
+
+  @action
+  choosingSpecificDay(event: { target: { value: Date } }) {
+    this.specificDay = event.target.value;
+    this.choosingDay = new Date(this.specificDay);
+  }
+
+  get todayFormat() {
+    let todayFormat = format(this.today, 'EEEE - d / LL');
+    return todayFormat;
+  }
+
   get columns() {
     let cols: number[] = Array(5);
     return cols;
@@ -14,33 +52,49 @@ export default class PagesDashboardWeek extends Component<PagesDashboardWeekArgs
   }
 
   get currentMonth() {
-    let today = new Date();
-    let numMonth: number = today.getMonth();
-    let months: string[] = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
-    ];
-    return months[numMonth];
+    let month: string = format(this.choosingDay, 'MMMM - yyyy');
+    return month;
   }
 
-  get today() {
-    let today = new Date();
-    let numDay: number = today.getDay();
+  get numDay() {
+    let numDay: number = this.today.getDay();
     return numDay;
   }
 
-  // get monday(){
-  //   let numday: number = this.today;
-  //   let x : number = 1%numday
-  // }
+  get monday() {
+    let monday = startOfWeek(this.choosingDay, {
+      weekStartsOn: 0,
+    });
+    monday.setDate(monday.getDate() + 1);
+    let result = format(monday, 'EEEE - d / MM');
+    return result;
+  }
+
+  get tuesday() {
+    let tuesday = startOfWeek(this.choosingDay, { weekStartsOn: 0 });
+    tuesday.setDate(tuesday.getDate() + 2);
+    let result = format(tuesday, 'EEEE - d / MM');
+    return result;
+  }
+
+  get wednesday() {
+    let wednesday = startOfWeek(this.choosingDay, { weekStartsOn: 0 });
+    wednesday.setDate(wednesday.getDate() + 3);
+    let result = format(wednesday, 'EEEE - d / MM');
+    return result;
+  }
+
+  get thursday() {
+    let thursday = startOfWeek(this.choosingDay, { weekStartsOn: 0 });
+    thursday.setDate(thursday.getDate() + 4);
+    let result = format(thursday, 'EEEE - d / MM');
+    return result;
+  }
+
+  get friday() {
+    let friday = startOfWeek(this.choosingDay, { weekStartsOn: 0 });
+    friday.setDate(friday.getDate() + 5);
+    let result = format(friday, 'EEEE - d / LL');
+    return result;
+  }
 }
