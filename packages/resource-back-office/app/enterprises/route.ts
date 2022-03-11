276bfebf6437cd3382905fcb10b5085c04b07ce1
AmauryD <deflorenne.amaury@gmail.com>
2022-03-11 10:52:07 +0100

    remarques pres

--- packages/resource-back-office/app/enterprises/route.ts
+++ packages/resource-back-office/app/enterprises/route.ts
@@ -1,6 +1,6 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
-import type Store from '@ember-data/Store';
+import type Store from '@ember-data/store';
 
 export default class Enterprises extends Route {
   @service declare store: Store;
